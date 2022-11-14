d08f3710a89230a817d958348bbaf55340616791
Eduardo M <contact@eduardom.dev>
2022-11-13 16:47:42 -0800

    Finish processing

--- utils/tree_map_processor.dart
+++ utils/tree_map_processor.dart
@@ -84,15 +84,23 @@ void main() async {
 
     print("-------------------------------");
 
+    if (addressIndex == -1 && rooms.isNotEmpty) {
+      aka = rooms.cast();
+      rooms = [];
+    }
+    if (aka.length == 1) {
+      aka.add("Unknown Name");
+    }
+
     treeRouteLocs.add(TreeRouteObj(
       geoloc: {
         "lat": loc["lat"],
         "lon": loc["lon"],
       },
-      slug: address == null ? rooms[0] : aka[0],
-      aka: address == null ? rooms : aka,
-      name: address == null ? rooms[1] : aka[1],
-      rooms: address == null ? null : rooms,
+      slug: aka[0],
+      aka: aka,
+      name: aka[1],
+      rooms: rooms,
       address: address ?? "Unknown Street",
     ));
   }
@@ -103,7 +111,8 @@ void main() async {
   final treeRuoteFile = File('treeRouteReady.json');
   final treeRouteWritableFile =
       await treeRuoteFile.open(mode: FileMode.writeOnly);
-  treeRouteWritableFile.writeStringSync(jsonEncode(treeRouteLocs));
+  treeRouteWritableFile.writeStringSync(jsonEncode(treeRouteLocs),
+      encoding: utf8);
   treeRouteWritableFile.closeSync();
 
   print("Complete, written to ${treeRuoteFile.path}");
@@ -184,15 +193,111 @@ class TreeRouteObj {
   TreeRouteObj({
     required this.name,
     required this.address,
+    this.state = "CA",
+    this.country = "US",
     this.aka,
     this.campus = 1,
-    this.country = "US",
+    required this.slug,
     required this.geoloc,
+    this.type,
     this.postalCode = 94305,
     this.departments,
     this.rooms,
-    required this.slug,
-    this.state = "CA",
-    this.type,
   });
+
+  TreeRouteObj copyWith({
+    String? name,
+    String? address,
+    String? state,
+    String? country,
+    List<String>? aka,
+    int? campus,
+    String? slug,
+    Map<String, double>? geoloc,
+    List<String>? type,
+    int? postalCode,
+    List<String>? departments,
+    List<String>? rooms,
+  }) {
+    return TreeRouteObj(
+      name: name ?? this.name,
+      address: address ?? this.address,
+      state: state ?? this.state,
+      country: country ?? this.country,
+      aka: aka ?? this.aka,
+      campus: campus ?? this.campus,
+      slug: slug ?? this.slug,
+      geoloc: geoloc ?? this.geoloc,
+      type: type ?? this.type,
+      postalCode: postalCode ?? this.postalCode,
+      departments: departments ?? this.departments,
+      rooms: rooms ?? this.rooms,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'address': address,
+      'state': state,
+      'country': country,
+      'aka': aka,
+      'campus': campus,
+      'slug': slug,
+      'geoloc': geoloc,
+      'type': type,
+      'postalCode': postalCode,
+      'departments': departments,
+      'rooms': rooms,
+    };
+  }
+
+  factory TreeRouteObj.fromMap(Map<String, dynamic> map) {
+    return TreeRouteObj(
+      name: map['name'] ?? '',
+      address: map['address'] ?? '',
+      state: map['state'] ?? '',
+      country: map['country'] ?? '',
+      aka: List<String>.from(map['aka']),
+      campus: map['campus']?.toInt() ?? 0,
+      slug: map['slug'] ?? '',
+      geoloc: Map<String, double>.from(map['geoloc']),
+      type: List<String>.from(map['type']),
+      postalCode: map['postalCode']?.toInt() ?? 0,
+      departments: List<String>.from(map['departments']),
+      rooms: List<String>.from(map['rooms']),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory TreeRouteObj.fromJson(String source) =>
+      TreeRouteObj.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'TreeRouteObj(name: $name, address: $address, state: $state, country: $country, aka: $aka, campus: $campus, slug: $slug, geoloc: $geoloc, type: $type, postalCode: $postalCode, departments: $departments, rooms: $rooms)';
+  }
+
+  @override
+  int get hashCode {
+    return name.hashCode ^
+        address.hashCode ^
+        state.hashCode ^
+        country.hashCode ^
+        aka.hashCode ^
+        campus.hashCode ^
+        slug.hashCode ^
+        geoloc.hashCode ^
+        type.hashCode ^
+        postalCode.hashCode ^
+        departments.hashCode ^
+        rooms.hashCode;
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    return super == other;
+  }
 }
